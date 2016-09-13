3ba9ff4a3649ab474ec4657ec41e6c3fdc6b3a40
deqncho <deqn__1996@abv.bg>
2016-09-13 23:16:01 +0300

    add a spider

---
+++ download.py
@@ -0,0 +1,2 @@
+import pdfkit
+pdfkit.from_url('http://www.inf.ed.ac.uk/teaching/courses/pi/2015_2016/index.html', 'out.pdf')
--- spider.py
+++ spider.py
@@ -2,29 +2,36 @@ import urlparse
 import urllib
 from bs4 import BeautifulSoup
 from pprint import pprint
-import requests
+import os
 
-url = "http://www.nytimes.com/"
+url = raw_input("[+] Enter the url: ")
+course_name = raw_input("[+] Enter the course name: ")
+complete_path = "/home/deyan/Desktop/KNOWLEDGEBASE/" + course_name
+if not os.path.exists(complete_path):
+    os.makedirs(complete_path)
+print(complete_path)
 
 urls = [url]
 visited = [url]
 
 while len(urls) > 0:
-
-    current_url  = urls.pop(0)
     try:
-        htmltext = urllib.urlopen(current_url.read())
+        htmltext = urllib.urlopen(urls[0]).read()
     except:
-        print current_url
-        continue
+        print urls[0]
     soup = BeautifulSoup(htmltext)
+    
+    current_url = urls.pop(0)
+    extension = os.path.splitext(os.path.basename(current_url))[1]
+    print "On page %s right now" %(current_url)
+    print "Extension: %s" %(extension)
+    print "========================="
 
+    print(len(urls))
 
-    print len(urls)
     for tag in soup.findAll('a', href = True):
         tag['href'] = urlparse.urljoin(url, tag['href'])
         if url in tag['href'] and tag['href'] not in visited:
             urls.append(tag['href'])
             visited.append(tag['href'])
-
-pprint(visited)
+            print(tag['href'])
